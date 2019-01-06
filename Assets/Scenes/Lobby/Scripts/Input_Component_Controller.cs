e024456604be66f9ef5c411f0919bca7d5ffb26e
Massimiliano <m.marchiano2@campus.unimib.it>
2019-01-06 23:49:14 +0100

    Added lobby

---
+++ Assets/Scenes/Lobby/Scripts/Input_Component_Controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input_Component_Controller : MonoBehaviour
+{
+    public UI_Controller UIScript;
+    public void start_Button_Click()
+    {
+
+        Debug.Log("Start Button has been clicked");
+    }
+    
+    public void exit_Button_Click()
+    {
+        Debug.Log("Exit Button has been clicked");
+    }
+
+    public void easy_Button_Click()
+    {
+        Debug.Log("Easy Button has been clicked");
+    }
+
+    public void medium_Button_Click()
+    {
+        Debug.Log("Medium Button has been clicked");
+    }
+
+    public void hard_Button_Click()
+    {
+        Debug.Log("Hard Button has been clicked");
+    }
+
+    public void next_Map_Button_Click()
+    {
+        Debug.Log("Next Button has been clicked");
+        UIScript.changeImage(true);
+    }
+  
+    public void previus_Map_Button_Click()
+    {
+        Debug.Log("Previous Button has been clicked");
+        UIScript.changeImage(false);
+    }
+}
