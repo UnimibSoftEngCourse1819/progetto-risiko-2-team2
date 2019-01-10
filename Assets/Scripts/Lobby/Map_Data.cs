bfe5cf6731600ccb2538af21bb380d2391d6bede
Gio-Valle <g.vallero@campus.unimib.it>
2019-01-10 10:46:56 +0100

    Sorted files, improved next and previus button (now they change the map's name and the inputField for the number of lands, created map_data)

---
+++ Assets/Scripts/Lobby/Map_Data.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map_Data : MonoBehaviour
+{
+    private string name;
+    private int maxEasy, maxNormal, maxHard, minEasy, minNormal, minHard;
+    private Sprite image;
+
+    public Map_Data(string name, int maxEasy, int maxNormal, int maxHard, int minEasy, int minNormal, int minHard, Sprite image)
+    {
+        this.name = name;
+        this.maxEasy = maxEasy;
+        this.maxNormal = maxNormal;
+        this.maxHard = maxHard;
+        this.minEasy = minEasy;
+        this.minNormal = minNormal;
+        this.minHard = minHard;
+        this.image = image;
+    }
+
+    public string getName()
+    {
+        return name;
+    }
+
+    public int getmaxEasy()
+    {
+        return maxEasy;
+    }
+
+    public int getmaxNormal()
+    {
+        return maxNormal;
+    }
+
+    public int getmaxHard()
+    {
+        return maxHard;
+    }
+
+    public int getminEasy()
+    {
+        return minEasy;
+    }
+
+    public int getminNormal()
+    {
+        return minNormal;
+    }
+
+    public int getminHard()
+    {
+        return minHard;
+    }
+
+    public Sprite getimage()
+    {
+        return image;
+    }
+
+}
