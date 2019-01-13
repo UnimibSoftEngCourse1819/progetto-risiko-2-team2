cc4d8aac4baae43e7a3023bb2f4804faec5feff6
Alberto <a.bellinzoni@campus.unimib.it>
2019-01-13 23:32:08 +0100

    mappa + crea file mappa
    adesso la mappa si vede e la crea funziona , c'è anch eil file della mappa 0

---
+++ fuck/Assets/scripts/land_installer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using UnityEngine;
+
+public class land_installer: MonoBehaviour
+{
+   
+    private Sprite land_sprite;
+    private PolygonCollider2D collider3;
+
+
+
+    void Start()
+    {
+        land_sprite = GameObject.Find("eu-1").GetComponent<Sprite>();
+  //  distruggere l'event collider
+    //     collider3 = GameObject.Find("eu-1").GetComponent<PolygonCollider2D>();
+   //     Destroy(collider3);
+        Debug.Log(land_sprite.ToString());
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
