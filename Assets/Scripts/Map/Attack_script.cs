d70e761ad60461325ffcacf832be774a4c85894f
Alberto <a.bellinzoni@campus.unimib.it>
2019-01-21 22:46:07 +0100

    aggiunta possibilità di attacare uno stato
    è un po brutto da vedere ma funziona

---
+++ Assets/Scripts/Map/Attack_script.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Attack_script : MonoBehaviour
+{
+    private Text testo;
+    private State_script attaccante;
+    private State_script attaccato;
+    private int att = 0;
+    private int vicini;
+
+    private void Awake()
+    {
+        testo =  GameObject.Find("Map_text").GetComponent<Text>();
+        testo.text = "Benvenuto nella mappa";
+    }
+
+    public void OnMouseDown() // solo quando è il turno di un giocatore
+    {
+        testo.text = "scegli uno stato da cui attaccare";
+        att=1;
+        // ovviamente lo stato da cui attaccare deve appartenere al giocatore
+        Debug.Log("pronto per attaccare");
+    }
+
+    public int GetAtt()
+    {
+        return att;
+    }
+
+    public void GetAttaccante(string codice) // salvo il territorio da cui attacacre
+    {
+        attaccante = GameObject.Find(codice).GetComponent<State_script>();
+        att=2;
+        testo.text = "seleziona uno stato da attaccare";
+    }
+
+    public void GetAttaccato(string codice) // territorio da attacare e conttrollo
+    {
+        attaccato = GameObject.Find(codice).GetComponent<State_script>();
+        vicini=attaccato.IslandNear(attaccante.GetCodice()); // guardo se sono sue territori vicini
+        if(vicini==-1)
+        {
+            att = 0;
+            testo.text = "I territori selezionati non sono vicini scegliene altri";
+        }
+    }
+
+    private void Update()
+    {
+        if ((vicini == 1) && (att == 2))  // eseguo un attacco
+        {
+            att = 0;
+            vicini = 0;
+            testo.text = "attacco da "+attaccante.GetName()+" a "+ attaccato.GetName()+" eseguito";
+        }
+    }
+
+
+
+}
--- Assets/Scripts/Map/State_script.cs
+++ Assets/Scripts/Map/State_script.cs
@@ -16,12 +16,38 @@ public class State_script : MonoBehaviour
     private string[] near_lands;
     private int Player_ID;
     private int N_tank;
+    private Attack_script attacco;
+    private int N_vicini=0;
 
-    private void OnMouseDown()
+    private void Awake()
     {
-        Debug.Log(land_name);
-        
+        attacco =  GameObject.Find("Attacca").GetComponent<Attack_script>();
     }
+
+    private void OnMouseDown() // passo allo script attack_script i codici degli stati
+    {         
+        if (attacco.GetAtt() == 1)
+        {
+            attacco.GetAttaccante(land_code);
+            Debug.Log("primo");
+        }
+        else if (attacco.GetAtt() == 2)
+        {
+            attacco.GetAttaccato(land_code);
+            Debug.Log("secondo");
+        }
+    }
+
+    public int IslandNear(string codice)  // controlla se un dato territorio è un vicino
+    {
+        int r=-1;
+        for (int i =0; i< N_vicini;i++)
+        {
+            if (near_lands[i] == codice)
+                r = 1;
+        }
+        return r;
+    } 
 
     public void Inizializza(string name, string c_name, string code)
     {
@@ -37,17 +63,18 @@ public class State_script : MonoBehaviour
 
     public void Inser_new_neighbour(string neighbour) // salvo il nuovo vicino
     {
-        for (int i = 0; i < MAX_BORDERS; i++)
-        {
-            if (near_lands[i] == "")
-            {
-                near_lands[i] = neighbour;
-                break;
-            }
-        }
+        near_lands[N_vicini] = neighbour;
+        N_vicini++;
     }
 
-
+    public string GetName()
+    {
+        return land_name;
+    }
+    public string GetCodice()
+    {
+        return land_code;
+    }
 
 
 }
